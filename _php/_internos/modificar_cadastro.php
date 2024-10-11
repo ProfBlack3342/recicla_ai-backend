40cb8b4cd42185dac4272826432237c59cd6e9d6
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2024-10-11 11:57:24 -0300

    Update modificar_cadastro.php

--- _php/_internos/modificar_cadastro.php
+++ _php/_internos/modificar_cadastro.php
@@ -3,13 +3,16 @@
     require_once 'interfaces.php';
     require_once 'classes.php';
 
+    session_start();
+
     $login = $_POST['login'];
     $senhaAtual = $_POST['senhaAtual'];
-    
-    
+    $hash = $_SESSION['usuario']->getSenha();
+    $isSenhaAtualCorreta = password_verify($senhaAtual, $hash);
+
     if(isset($_POST['atualizar'])) {    // Atualiza usuário se o botão Atualizar for clicado
 
-        if(password_verify($senhaAtual, $_SESSION[UsuarioVO::getNomesColunasTabela[3]])) {
+        if($isSenhaAtualCorreta) {
         
             $senhaNova1 = null;
             $senhaNova2 = null;
@@ -21,11 +24,13 @@
                 if($senhaNova1 !== $senhaNova2) {
                     echo"<script>
                         alert('As senhas novas não são iguais entre si');
-                        window.location.href = '../sessao.php';
+                        window.location.href = '../_publicos/sessao.php';
                     </script>";
                     exit();
                 }
             }
+            else
+                $senhaNova1 = $senhaAtual;
 
             $nome = null;
             if(array_key_exists('nome', $_POST)) 
@@ -48,16 +53,15 @@
             if(empty($tentativaEdicao)) {
                 echo"<script>
                         alert('Erro na edição!');
-                        window.location.href = '../sessao.php';
+                        window.location.href = '../_publicos/sessao.php';
                     </script>";
                 exit();
             }
             else {
-                fazerLogoff();
-                fazerLogin($login, $senhaNova);
+                $_SESSION['usuario'] = $usuarioVO;
                 echo"<script>
                         alert('Cadastro atualizado com sucesso!');
-                        window.location.href = '../sessao.php';
+                        window.location.href = '../_publicos/sessao.php';
                     </script>";
                 exit();
             }
@@ -65,20 +69,21 @@
         else {
             echo"<script>
                     alert('Senha atual incorreta!');
-                    window.location.href = '../sessao.php';
+                    window.location.href = '../_publicos/sessao.php';
                 </script>";
             exit();
         }
     }
     elseif(isset($_POST['excluir'])) {  // Excluir usuário se o botão Excluir for clicado
-        if(password_verify($senhaAtual, $_SESSION[UsuarioVO::getNomesColunasTabela[3]])) {
 
-            $tentativaRemocao = FactoryServicos::getServicosUsuario()->deletarUsuario($_SESSION[UsuarioVO::getNomesColunasTabela[0]]);
+        if($isSenhaAtualCorreta) {
+
+            $tentativaRemocao = FactoryServicos::getServicosUsuario()->deletarUsuario($_SESSION['usuario']->getId());
     
             if(empty($tentativaRemocao)) {
                 echo"<script>
                         alert('Erro na remoção!');
-                        window.location.href = '../sessao.php';
+                        window.location.href = '../_publicos/sessao.php';
                     </script>";
                 exit();
             }
@@ -86,7 +91,7 @@
                 fazerLogoff();
                 echo"<script>
                         alert('Cadastro removido com sucesso! Faça login novamente...');
-                        window.location.href = '../sessao.php';
+                        window.location.href = '../_publicos/sessao.php';
                     </script>";
                 exit();
             }
@@ -94,7 +99,7 @@
         else {
             echo"<script>
                     alert('Senha incorreta!');
-                    window.location.href = '../sessao.php';
+                    window.location.href = '../_publicos/sessao.php';
                 </script>";
             exit();
         }
@@ -103,7 +108,7 @@
         fazerLogoff();
         echo"<script>
                 alert('Saindo do usuário atual');
-                window.location.href = '../sessao.php';
+                window.location.href = '../_publicos/sessao.php';
             </script>";
         exit();
     }
