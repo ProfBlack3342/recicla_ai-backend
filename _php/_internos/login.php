9b329420bdddf4b13a2b1866ad649169d72ebee5
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2024-10-29 12:20:01 -0300

    Update login.php

--- _php/_internos/login.php
+++ _php/_internos/login.php
@@ -9,12 +9,20 @@
     $host = $_SERVER['HTTP_HOST'];
     $uri = rtrim(dirname(dirname($_SERVER['PHP_SELF'])), '/\\');
 
-    if(fazerLogin($login, $senha)) {
-        header("Location: http://$host$uri/sessao.php", true);
-        exit('Dados corretos, fazendo login...');
+    try {
+        if(fazerLogin($login, $senha)) {
+            header("Location: http://$host$uri/sessao.php", true);
+            exit('Dados corretos, fazendo login...');
+        }
+        else {
+            header("Location: http://$host$uri/home.php", true);
+            exit('Usuário e/ou senha incorretos!');
+        }
     }
-    else {
-        header("Location: http://$host$uri/home.php", true);
-        exit('Usuário e/ou senha incorretos!');
+    catch(MySQLException $sqle) {
+        $stringEx = 'Exceção encontrada: ' . $sqle->getMessage();
+        echo "<script> alert('$stringEx'); </script>";
+        echo "<script> window.location.href = 'http://$host$uri/sessao.php'; </script>";
     }
+    
 ?>
