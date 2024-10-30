3f0174dc715bca2093af2e383584b2bc41f8483f
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2024-10-30 12:27:36 -0300

    Update entrar.php

--- _php/entrar.php
+++ _php/entrar.php
@@ -4,7 +4,7 @@
 
     session_start();
 
-    if(array_key_exists('usuario', $_SESSION) && ($_SESSION('usuario') instanceof UsuarioVO)) {
+    if(array_key_exists('usuario', $_SESSION) && ($_SESSION['usuario'] instanceof UsuarioVO)) {
         fazerLogoff();
         session_start();
     }
